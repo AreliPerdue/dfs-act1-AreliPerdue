60be7d9001b4aaa95d47b4a8b305d32ecfc7b29d
Areli Perdue <perdue.areli28@gmail.com>
2026-02-16 00:46:38 -0600

    git fix2

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,3 +3,19 @@ export default {
     transform: { '^.+\\.js$': 'babel-jest' },
     setupFiles: ['dotenv/config']
 }
+module.exports = {
+projects: [
+    {
+        displayName: 'backend',
+        testMatch: ['<rootDir>/backend/tests/**/*.test.js'],
+        testEnvironment: 'node',
+        transform: {}
+    },
+    {
+        displayName: 'frontend',
+        testMatch: ['<rootDir>/tests/**/*.test.js'],
+        testEnvironment: 'jsdom',
+        transform: {}
+    }
+    ]
+};
